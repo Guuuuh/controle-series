691a729e68a1f7415a85f0cd4698815aef016d96
Guuuuh <gustavo.htl@hotmail.com>
2023-04-08 19:17:17 -0300

    Metodo para excluir series

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,9 +1,25 @@
 <x-layout title="Séries">
-    <a class="btn btn-dark mb-2" href="{{ route('series.create') }}">Adicionar Série</a>
+    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar Série</a>
+
+    @isset($mensagemDeRemocao)
+    <div class="alert alert-success">
+        {{ $mensagemDeRemocao }}
+    </div>
+    @endisset
 
     <ul class="list-group">
         @foreach ($series as $serie)
-        <li class="list-group-item">{{ $serie->nome }}</li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            {{ $serie->nome }}
+
+            <form action="{{ route('series.destroy', $serie->id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-sm">
+                    X
+                </button>
+            </form>
+        </li>
         @endforeach
     </ul>
 </x-layout>
